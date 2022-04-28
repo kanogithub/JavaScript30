c799af6be029b7b1aa9f1fe4862c79ed63f4e8a9
kanogithub <linkano.teck@gmail.com>
2022-04-29 03:15:44 +1000

    Type Ahead v.2

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -6,10 +6,6 @@
 		population: city.population,
 	}))
 
-	const numberWithCommas = (x) => {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-	}
-
 	const searchBar = document.querySelector('.search')
 	const suggestions = document.querySelector('.suggestions')
 
@@ -21,9 +17,7 @@
 		const outputElems = results
 			.map(({ name, population }) => {
 				const cityElem = name.replaceAll(regex, `<span class='hl'>${value}</span>`)
-				const populcationElem = `<span class='population'>${numberWithCommas(
-					population
-				)}</span>`
+				const populcationElem = `<span class='population'>${(+population).toLocaleString()}</span>`
 
 				const replacedElem = elemStr.replace('{city}', cityElem)
 				return replacedElem.replace('{population}', populcationElem)
